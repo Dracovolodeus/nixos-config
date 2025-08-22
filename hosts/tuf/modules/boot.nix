45a09040100fd84caaafd686cb4726261c8da359
Dracovolodeus <bananaed12@gmail.com>
2025-08-22 19:45:10 +0700

    change systemd-boot to grub

--- hosts/tuf/modules/boot.nix
+++ hosts/tuf/modules/boot.nix
@@ -1,4 +1,12 @@
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    # systemd-boot.enable = false;  # Явно отключаем systemd-boot
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      efiSupport = true;
+      useOSProber = true;
+    };
+  };
 }
