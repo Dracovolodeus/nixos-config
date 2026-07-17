ab0d1a4743305c9e0cf81f6cfb46f536f2a3b8df
Dracovolodeus <bananaed12@gmail.com>
2026-07-17 12:28:33 +0700

    Add mpris to mpv.

--- features/modules/home-manager/programs/gui/media/mpv/default.nix
+++ features/modules/home-manager/programs/gui/media/mpv/default.nix
@@ -1,6 +1,7 @@
-{
+{ pkgs, ... }: {
   programs.mpv = {
     enable = true;
+    scripts = with pkgs.mpvScripts; [ mpris ];
     bindings = {
       "]" = "add speed 0.25";
       "[" = "add speed -0.25";
