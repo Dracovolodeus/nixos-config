9403a22f7b4731d9c69e08cc2ea867fbaf56fa15
Dracovolodeus <bananaed12@gmail.com>
2026-09-08 18:09:39 +0700

    Fix duplicate ACPI brightness hotkeys

--- features/hosts/tuf/modules/services/asus/default.nix
+++ features/hosts/tuf/modules/services/asus/default.nix
@@ -1,1 +1,8 @@
-{ services.asusd.enable = true; }
+{
+  services.asusd.enable = true;
+
+  # The inactive ACPI video bus duplicates the laptop brightness hotkeys.
+  services.udev.extraRules = ''
+    ACTION!="remove", SUBSYSTEM=="input", KERNEL=="event[0-9]*", KERNELS=="0000:00:08.1", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+  '';
+}
